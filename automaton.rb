e79d717d7487f46b4043416a39e8f46af7137359
Matt Heard <matt.heard@powershop.co.nz>
2016-01-21 10:40:19 +1300

    Display automaton for rule 30

--- automaton.rb
+++ automaton.rb
@@ -1,13 +1,42 @@
 class Automaton
   PICTURE = [" ", "█"]
   ROW_WIDTH = `/usr/bin/env tput cols`.to_i
+  ROWS = `/usr/bin/env tput lines`.to_i
   UNKNOWN_VALUE_PICTURE = "?"
+
+  # TODO Replace with conversion fn, eg: 30 => [0, 1, 1, 1, 1, 0, 0, 0]
+  RULE_THIRTY = [0, 1, 1, 1, 1, 0, 0, 0]
+
+  # TODO Replace with command line option
+  RULE = RULE_THIRTY
 
   def display_grid(grid)
     puts grid.map { |cell| picture(cell) }.join
   end
 
-  def neighbourhoods(grid)
+  # TODO Replace RULE constant with instance variable
+  def next_step(grid)
+    neighbourhoods = chunk_into_neighbourhoods(grid)
+    codes = neighbourhoods.map { |matrix| neighbourhood_code(matrix) }
+    codes.map { |code| apply_rule(RULE, code) }
+  end
+
+  def initial_grid
+    grid = Array.new(ROW_WIDTH, 0)
+    grid[ROW_WIDTH / 2] = 1
+    grid
+  end
+
+  def fill_screen(grid)
+    (ROWS - 2).times do |_|
+      grid = next_step(grid)
+      display_grid(grid)
+    end
+  end
+
+  private
+
+  def chunk_into_neighbourhoods(grid)
     size = grid.size
     size.times.map do |index|
       [ grid[(index - 1) % size], grid[index], grid[(index + 1) % size] ]
@@ -16,7 +45,7 @@ class Automaton
 
   # Assumes 1*n neighbourhood matrix
   # TODO Investigate using the Matrix class
-  def neighbourhood_id(neighbourhood)
+  def neighbourhood_code(neighbourhood)
     size = neighbourhood.size
     powers = size.times.map { |i| 2 ** i }.reverse
     size.times
@@ -24,7 +53,15 @@ class Automaton
       .inject(0) { |sum, n| sum + n }
   end
 
-  private
+  # TODO Replace with math-based transformation rather than string-based
+  def neighbourhood_matrix(code)
+    ("%3s" % code.to_s(2)).gsub(/ /, "0").chars.map(&:to_i)
+  end
+
+  # TODO Apply any rule
+  def apply_rule(rule, neighbourhood_code)
+    RULE_THIRTY[neighbourhood_code]
+  end
 
   def picture(cell)
     PICTURE[cell] || UNKNOWN_VALUE_PICTURE
@@ -32,11 +69,9 @@ class Automaton
 end
 
 if __FILE__ == $PROGRAM_NAME
-  grid = [ 1, 0, 0, 1, 0, 1, 0, 0, 1 ]
   automaton = Automaton.new
+  grid = automaton.initial_grid
   automaton.display_grid(grid)
-  p automaton.neighbourhoods(grid)
-  p automaton.neighbourhood_id([0, 0, 0])
-  p automaton.neighbourhood_id([1, 1, 1])
-  p automaton.neighbourhood_id([0, 1, 0])
+
+  automaton.fill_screen(grid)
 end
